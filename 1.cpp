30d52b0568482e746bfa356e088dd9eefce8f64d
unknown <https://github.com/Sewerpl/projekt-1.git>
2024-10-19 16:27:19 +0200

    1 commit

---
+++ 1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* prev;
+    Node* next;
+
+    Node(int val) : data(val), prev(nullptr), next(nullptr) {}
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+ 
+    return 0;
+}
